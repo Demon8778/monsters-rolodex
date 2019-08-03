32b0d712327de49cba4bd6d790decb4c641fee6e
Kirankumar Ahir <kirankatariya8778@gmail.com>
2019-08-04 04:10:23 +0530

    Updates

---
+++ precache-manifest.32f257ba1e461e54973afbfa80d551ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdfbd6c853b1e3bea500d8310cd3e26c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3145482f02f8486a082b",
+    "url": "/monsters-rolodex/static/css/main.cc9622d9.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "3145482f02f8486a082b",
+    "url": "/monsters-rolodex/static/js/main.8901a09b.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
